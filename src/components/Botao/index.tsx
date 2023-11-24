9312ae4ede8f1f120fd2898487efa588b3419f17
mateussfortes <mateussilveirafortes@gmail.com>
2023-11-24 17:59:13 -0300

    CSS modules

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import style from './Botao.module.scss';
+
 class Botao extends React.Component {
     render() {
         return (
-            <button>Botão</button>
+            <button className={style.botao}>Botão</button>
         )
     }
 }
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Botao from "../Botao";
 
+import style from './Formulario.module.scss';
+
 class Formulario extends React.Component {
     render() {
         return (
-            <form>
-                <div>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
@@ -18,7 +20,7 @@ class Formulario extends React.Component {
                         required
                     />
                 </div>
-                <div>
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
